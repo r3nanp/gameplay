91c32d6e2b6d2466b505ea420cba7cd2d38aeb42
Renan Pereira <renan.souza.dev@gmail.com>
2022-04-10 16:42:07 -0300

    feat(Home): create home screen

---
+++ src/screens/Home/Home.screen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './home.screen.styles'
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo</Text>
+    </View>
+  )
+}
